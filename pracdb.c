7fc0cc8e4a4b546457ee7c68fc73d30b8b07267f
Divesh Kamble <diveshkamble@gmail.com>
2023-02-19 14:23:52 +0000

    Added a meta command handler and insert,select handler

--- pracdb.c
+++ pracdb.c
@@ -11,6 +11,25 @@ typedef struct
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+}MetaCommandResult;
+
+typedef enum{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+}PrepareResult;
+
+typedef enum{
+    STATEMENT_SELECT,
+    STATEMENT_INSERT
+}StatementType;
+
+typedef struct{
+    StatementType type;
+}Statement;
+
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -50,6 +69,40 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer* input_buffer){
+    if(strcmp(input_buffer->buffer , ".exit") == 0){
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
+    if (strncmp(input_buffer->buffer , "insert",6) == 0){
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if(strcmp(input_buffer->buffer , "select")==0){
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void exucute_statement(Statement* statement){
+    switch(statement->type){
+        case(STATEMENT_INSERT):
+        printf("Perform an insert statement.\n");
+        break;
+        case(STATEMENT_SELECT):
+        printf("Perform a select statement.\n");
+        break;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
@@ -58,14 +111,28 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
-        {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            printf("Unrecognized command '%s' .\n", input_buffer->buffer);
-        }
+        
+            if(input_buffer->buffer[0] == '.')
+            {
+                switch(do_meta_command(input_buffer))
+                {
+                    case (META_COMMAND_SUCCESS):
+                    continue;
+                    case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'./n",input_buffer->buffer);
+                    continue;
+                }
+            }
+            Statement statement;
+            switch(prepare_statement(input_buffer,&statement)){
+            case(PREPARE_SUCCESS):
+            break;
+            case(PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyboard at start of '%s'.\n",input_buffer->buffer);
+            continue;
     }
+    exucute_statement(&statement);
+        printf("Executed.\n");
+    }
+        
 }
