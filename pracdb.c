f08137ef9cdb8b520cbf254042713fc62219e3f8
Divesh Kamble <diveshkamble@gmail.com>
2023-03-12 04:59:33 +0000

    Implemented insert functionality and row serializer and deserializer

--- pracdb.c
+++ pracdb.c
@@ -3,6 +3,22 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
+#define COLUMN_EMAIL_SIZE 255
+#define COLUMN_USERNAME_SIZE 32
+// ((Struct *)0)->Attribute is used to access the value of the 'Attribute' member of a structure 'Struct' through a pointer to the structure
+// sizeof(((Struct *)0)->Attribute) is the same as the size of the data type of the 'Attribute' member.
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+
+// Since the structure passed here is Row; the size of ID_SIZE will be 4 bytes(int) because the attribute which is
+// being passed here is id which is of type uint32_t
+const uint32_t ID_SIZE = size_of_attribute(Row,id)
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
 
 typedef struct
 {
@@ -18,7 +34,8 @@ typedef enum{
 
 typedef enum{
     PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT
+    PREPARE_UNRECOGNIZED_STATEMENT,
+    PREPARE_SYNTAX_ERROR
 }PrepareResult;
 
 typedef enum{
@@ -28,7 +45,14 @@ typedef enum{
 
 typedef struct{
     StatementType type;
+    Row row_to_insert;
 }Statement;
+
+typedef struct{
+uint32_t id;
+char username[COLUMN_USERNAME_SIZE];
+char email[COLUMN_EMAIL_SIZE]
+}Row
 
 InputBuffer *new_input_buffer()
 {
@@ -43,6 +67,21 @@ InputBuffer *new_input_buffer()
 void print_prompt()
 {
     printf("pracdb > ");
+}
+
+void serialize_row(Row* source, void* destination)
+{
+    memcpy(destination + ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination + USERNAME_OFFSET, &(source->username), USERNAME_SIZE);
+    memcpy(destination + EMAIL_OFFSET, &(source->email), EMAIL_SIZE);
+}
+
+void deserialize_row(void * source, Row* destination)
+{
+    memcpy(&(destination->id), source + ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source + USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source + EMAIL_OFFSET, EMAIL_SIZE);
+
 }
 
 void read_input(InputBuffer *input_buffer)
@@ -82,6 +121,11 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer){
 PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement){
     if (strncmp(input_buffer->buffer , "insert",6) == 0){
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s",&(statement->row_to_insert.id),&(statement->row_to_insert.username),&(statement->row_to_insert.email) );
+        if(args_assigned>3)
+        {
+            return PREPARE_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
     if(strcmp(input_buffer->buffer , "select")==0){
